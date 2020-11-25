e79cfed2df19d1d36634fbd18f05e58b9da588bb
DanParSeg <44481578+DanParSeg@users.noreply.github.com>
2020-11-25 14:21:19 +0100

    sincronizarhora.bat

---
+++ sicronizarhora.bat
@@ -0,0 +1,15 @@
+@echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+@echo on
+
+net start w32time 2> nul
+
+@echo off
+timeout /t 1 /nobreak > nul
+@echo on
+
+w32tm /resync
+
+@echo off
+exit
